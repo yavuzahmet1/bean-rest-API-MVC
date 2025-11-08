399a7c30ec97e3d20ee42f1b4a75fd899d573078
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-08 19:18:39 +0300

    feat: add doSomething method to UserService

--- bean-lesson/src/main/java/com/spring_core/bean_lesson/services/UserService.java
+++ bean-lesson/src/main/java/com/spring_core/bean_lesson/services/UserService.java
@@ -2,6 +2,8 @@ package com.spring_core.bean_lesson.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.spring_core.bean_lesson.model.User;
 
 import lombok.Getter;
@@ -9,6 +11,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Service
 public class UserService {
     private List<User> userList;
+
+    public void doSomething() {
+        System.out.println("UserService is doing something.");
+    }
 }
