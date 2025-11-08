a2569a6ed746fccd944b730d01f41fb71b385ea5
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-08 19:17:34 +0300

    refactor: implement proper Spring context management in MainClass

--- bean-lesson/src/main/java/com/spring_core/bean_lesson/main/MainClass.java
+++ bean-lesson/src/main/java/com/spring_core/bean_lesson/main/MainClass.java
@@ -1,6 +1,5 @@
 package com.spring_core.bean_lesson.main;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.spring_core.bean_lesson.config.AppConfig;
@@ -10,20 +9,20 @@ import com.spring_core.bean_lesson.services.UserService;
 
 
 public class MainClass {
-    public static void main(String[] args) {
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
-
-        for (User user : userService.getUserList()) {
-            System.out.println(user.toString());
-        }
+public static void main(String[] args) {
         
-        LoginService loginService = new LoginService();
-        loginService.login();
-
-  
-        
-        
-    }
-}
+       try (AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext(AppConfig.class)) {
+            
+            UserService userService = context.getBean(UserService.class);
+            
+            for (User user : userService.getUserList()) {
+                System.out.println(user.toString());
+            }
+            
+            // Artık bean olarak alıyoruz
+            LoginService loginService = context.getBean(LoginService.class);
+            loginService.login();
+            
+        } // Context otomatik kapanır
+}}
