006f9e8839c1284a367122e994d940df8391d51c
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-08 19:17:57 +0300

    refactor: implement proper Spring dependency injection in LoginService

--- bean-lesson/src/main/java/com/spring_core/bean_lesson/services/LoginService.java
+++ bean-lesson/src/main/java/com/spring_core/bean_lesson/services/LoginService.java
@@ -1,15 +1,23 @@
 package com.spring_core.bean_lesson.services;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.spring_core.bean_lesson.config.AppConfig;
+import org.springframework.stereotype.Service;
 
+
+
+@Service
 public class LoginService {
-
-    public void login() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
+    
+    private final UserService userService;
+    
+    // Constructor Injection
+    public LoginService(UserService userService) {
+        this.userService = userService;
     }
     
+    public void login() {
+        // UserService direkt kullanılabilir
+        userService.doSomething();
+        System.out.println("Login proccess executed.");
+    }
 }
