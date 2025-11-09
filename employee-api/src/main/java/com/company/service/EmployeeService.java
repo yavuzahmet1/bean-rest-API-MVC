9f3136a40d20130f59fae1959b40d7da25445d98
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-09 23:10:06 +0300

    refactor: implement employee service with repository layer
    - Update saveEmployee to use employeeRepository.save()
    - Modify deleteEmployee to check existence before deletion
    - Change updateEmployee to delegate to repository
    - Add proper null checking and boolean return handling

--- employee-api/src/main/java/com/company/service/EmployeeService.java
+++ employee-api/src/main/java/com/company/service/EmployeeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.company.model.Employee;
+import com.company.model.UpdateEmployeeRequest;
 import com.company.repository.EmployeeRepository;
 
 @Service
@@ -26,5 +27,23 @@ public class EmployeeService {
     public List<Employee> getEmployeeWithParams(String firstName, String lastName) {
         return employeeRepository.getEmployeesWithParams(firstName, lastName);
     }
+
+    public Employee saveEmployee(Employee newEmployee) {
+        return employeeRepository.save(newEmployee);
+    }
+
+public boolean deleteEmployee(String id) {
+    Employee employee = employeeRepository.getEmployeeById(id);
+    if (employee != null) {
+        employeeRepository.deleteEmployee(id); 
+        return true;
+    }
+    return false;
+}
+
+public Employee updateEmployee(String id,UpdateEmployeeRequest requestEmployee){
+    
+    return employeeRepository.updateEmployee(id, requestEmployee);
+}
 
 }
