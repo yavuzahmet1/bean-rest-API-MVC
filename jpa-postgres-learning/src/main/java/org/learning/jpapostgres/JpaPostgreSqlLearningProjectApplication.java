dfa6c7d1f30d4718c228c0a8f66531283aa6cf88
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-10 20:12:05 +0300

    feat: initial Spring Boot project setup with JPA and PostgreSQL

---
+++ jpa-postgres-learning/src/main/java/org/learning/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package org.learning.jpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPostgreSqlLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPostgreSqlLearningProjectApplication.class, args);
+	}
+
+}
