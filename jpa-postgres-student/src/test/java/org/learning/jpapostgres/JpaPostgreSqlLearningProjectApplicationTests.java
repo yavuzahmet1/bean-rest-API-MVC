9df6def3a81300e6e32481bd99175cb6ead7b045
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-12 18:55:46 +0300

    inifeat: initialize student management project

---
+++ jpa-postgres-student/src/test/java/org/learning/jpapostgres/JpaPostgreSqlLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.jpapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPostgreSqlLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
