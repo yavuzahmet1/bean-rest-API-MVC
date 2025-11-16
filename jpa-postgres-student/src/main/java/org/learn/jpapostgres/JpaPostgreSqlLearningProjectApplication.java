4d7742ca1e355cc45673fc544db3e3be2526612d
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-16 23:38:14 +0300

    refactor: change base package name from 'learning' to 'learn'

--- jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
+++ jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
@@ -1,4 +1,4 @@
-package org.learning.jpapostgres;
+package org.learn.jpapostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
