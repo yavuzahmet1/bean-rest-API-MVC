693b33e9459cec5ec921dfa419027a356eb32051
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-17 22:02:24 +0300

    refactor: Update Student ID generation strategy to IDENTITY

--- jpa-postgres-student/src/main/java/org/learn/entities/Student.java
+++ jpa-postgres-student/src/main/java/org/learn/entities/Student.java
@@ -1,5 +1,7 @@
 package org.learn.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,6 +19,7 @@ import jakarta.persistence.Column;
 public class Student {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name",nullable = false,length = 50)
