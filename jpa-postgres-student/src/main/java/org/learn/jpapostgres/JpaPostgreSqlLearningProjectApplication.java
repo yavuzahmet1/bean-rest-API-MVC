62dfb522e497bf5943ad83999ad6db7eeaebae15
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-17 22:02:55 +0300

    build: Explicitly define entity scanning package in main application

--- jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
+++ jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
@@ -2,7 +2,9 @@ package org.learn.jpapostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"org.learn.entities"})
 @SpringBootApplication
 public class JpaPostgreSqlLearningProjectApplication {
 
