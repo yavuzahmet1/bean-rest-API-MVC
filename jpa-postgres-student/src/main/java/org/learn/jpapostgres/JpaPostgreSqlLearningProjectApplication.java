2997f0efbbb55d91206a69372da377dadc3c9ab9
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-18 22:00:33 +0300

    chore(config): Configure Spring Boot component scanning and JPA repositories

--- jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
+++ jpa-postgres-student/src/main/java/org/learn/jpapostgres/JpaPostgreSqlLearningProjectApplication.java
@@ -3,9 +3,14 @@ package org.learn.jpapostgres;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"org.learn.entities"})
+
 @SpringBootApplication
+@EntityScan(basePackages = {"org.learn.entities"})
+@ComponentScan(basePackages = {"org.learn.controller", "org.learn.services", "org.learn.repository"})
+@EnableJpaRepositories(basePackages = {"org.learn.repository"})
 public class JpaPostgreSqlLearningProjectApplication {
 
 	public static void main(String[] args) {
