374efda57eb7ea2218dc9bb9cdca086bbaf5ab78
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-19 20:48:17 +0300

    feat(service): Add read and delete methods to IStudentService

--- jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
+++ jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
@@ -1,9 +1,14 @@
 package org.learn.services;
 
+import java.util.List;
+
 import org.learn.entities.Student;
 
 public interface IStudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+    public Student getStudentById(Long id);
+    public void deleteStudentById(Long id);
 
 }
