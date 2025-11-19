7cf4cb1987d524f63e8dabf32fc4cfd99082dd9c
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-19 20:47:06 +0300

    feat(controller): Add CRUD read and delete methods to IStudentController

--- jpa-postgres-student/src/main/java/org/learn/controller/IStudentController.java
+++ jpa-postgres-student/src/main/java/org/learn/controller/IStudentController.java
@@ -1,8 +1,13 @@
 package org.learn.controller;
 
+import java.util.List;
+
 import org.learn.entities.Student;
 
 public interface IStudentController {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+    public Student getStudentById(Long id);
+    public void deleteStudentById(Long id);
 }
