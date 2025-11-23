d4ae15d97b84cf2ac3254d261c6922fa8d31d653
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-23 11:50:04 +0300

    refactor(controller): integrate DTOs into IStudentController interface

--- jpa-postgres-student/src/main/java/org/learn/controller/IStudentController.java
+++ jpa-postgres-student/src/main/java/org/learn/controller/IStudentController.java
@@ -2,12 +2,14 @@ package org.learn.controller;
 
 import java.util.List;
 
+import org.learn.dto.DtoStudent;
+import org.learn.dto.DtoSudentIU;
 import org.learn.entities.Student;
 
 public interface IStudentController {
 
-    public Student saveStudent(Student student);
-    public List<Student> getAllStudents();
+    public DtoStudent saveStudent(DtoSudentIU dtoStudent);
+    public List<DtoStudent> getAllStudents();
     public Student getStudentById(Long id);
     public void deleteStudentById(Long id);
     public Student updateStudent(Long id, Student student);
