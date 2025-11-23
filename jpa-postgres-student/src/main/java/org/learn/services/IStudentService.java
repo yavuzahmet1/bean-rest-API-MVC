6cb20a0e9ed480d9f2742597ff95010f637c29e5
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-23 11:52:53 +0300

    refactor(service): adopt DTO pattern in IStudentService interface

--- jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
+++ jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
@@ -2,13 +2,15 @@ package org.learn.services;
 
 import java.util.List;
 
+import org.learn.dto.DtoStudent;
+import org.learn.dto.DtoSudentIU;
 import org.learn.entities.Student;
 
 public interface IStudentService {
 
-    public Student saveStudent(Student student);
-    public List<Student> getAllStudents();
-    public Student getStudentById(Long id);
+    public DtoStudent saveStudent(DtoSudentIU student);
+    public List<DtoStudent> getAllStudents();
+    public DtoStudent getStudentById(Long id);
     public void deleteStudentById(Long id);
     public Student updateStudent(Long id, Student student);
 
