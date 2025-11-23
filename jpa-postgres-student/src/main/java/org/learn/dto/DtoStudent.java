4ab6b60a9dab7886d3b79c29181de542fca9402a
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-23 11:51:39 +0300

    feat(dto): create DtoStudent class for response data

---
+++ jpa-postgres-student/src/main/java/org/learn/dto/DtoStudent.java
@@ -0,0 +1,13 @@
+package org.learn.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoStudent {
+    private String firstName;
+    private String lastName;
+}
