360eb33bd2b1940844d681871692fc14335be7c2
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-26 21:46:08 +0300

    feat(service): define IStudentService interface with DTOs

--- jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
+++ jpa-postgres-student/src/main/java/org/learn/services/IStudentService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.learn.dto.DtoStudent;
 import org.learn.dto.DtoSudentIU;
-import org.learn.entities.Student;
 
 public interface IStudentService {
 
@@ -12,6 +11,6 @@ public interface IStudentService {
     public List<DtoStudent> getAllStudents();
     public DtoStudent getStudentById(Long id);
     public void deleteStudentById(Long id);
-    public Student updateStudent(Long id, Student student);
+    public DtoStudent updateStudent(Long id, DtoSudentIU dtoSudentIU);
 
 }
