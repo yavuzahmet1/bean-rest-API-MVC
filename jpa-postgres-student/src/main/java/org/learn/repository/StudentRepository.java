877ce627d5cf3a0a6a1f9c75691cf9f8ad071b91
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-11-30 14:59:02 +0300

    feat(database): create StudentRepository for JPA operations

--- jpa-postgres-student/src/main/java/org/learn/repository/StudentRepository.java
+++ jpa-postgres-student/src/main/java/org/learn/repository/StudentRepository.java
@@ -1,10 +1,18 @@
 package org.learn.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.learn.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value="select * from students.students",nativeQuery = true)
+    List<Student> findAllStudents();
+    @Query(value="from Student s where s.id=:studentId")  
+    Optional<Student> findStudentById(Long studentId);
 
 }
