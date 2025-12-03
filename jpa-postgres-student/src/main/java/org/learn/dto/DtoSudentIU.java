af1bd6a66e4099b95a276f53bb5b159f295707d2
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-12-03 18:55:31 +0300

    feat(validation): add validation annotations to DtoSudentIU fields

--- jpa-postgres-student/src/main/java/org/learn/dto/DtoSudentIU.java
+++ jpa-postgres-student/src/main/java/org/learn/dto/DtoSudentIU.java
@@ -1,7 +1,11 @@
 package org.learn.dto;
 
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size; // Doğru import
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DtoSudentIU {
+
+    @NotEmpty(message = "First name must not be empty")
+    @Size(min = 2, max = 50, message = "First name must have between 2 and 50 characters")
     private String firstName;
+
+    @Size(min = 2, max = 50, message = "Last name must have between 2 and 50 characters")
+    @NotNull(message = "Last name must not be null")
     private String lastName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date birthOfDate;
 }
