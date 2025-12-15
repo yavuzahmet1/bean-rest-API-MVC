6df5e3d728e327a8e602c254ea82bbb971617153
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-12-15 21:13:53 +0300

    feat(entity): create Address entity with JPA annotations

---
+++ jpa-postgres-student/src/main/java/org/learn/entities/Address.java
@@ -0,0 +1,30 @@
+package org.learn.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @OneToOne(mappedBy = "address") 
+    private Customer customer;
+}
