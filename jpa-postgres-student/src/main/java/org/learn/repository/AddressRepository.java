b13d49d77a261e7ee46f25bd86a80be5be0c6701
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-12-15 21:14:06 +0300

    feat(repository): create AddressRepository interface for Address entity

---
+++ jpa-postgres-student/src/main/java/org/learn/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package org.learn.repository;
+
+import org.learn.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
